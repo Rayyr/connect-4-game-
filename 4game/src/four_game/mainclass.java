3560264e39d700e056851f2b3755f3e571f4edc8
Rayyr <khasaatirayaa@gmail.com>
2024-05-26 19:27:58 +0300

    2nd

--- 4game/src/four_game/mainclass.java
+++ 4game/src/four_game/mainclass.java
@@ -5,7 +5,7 @@ public class mainclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		game test_obj_game;
-	 
+	 ddd
 		test_obj_game=new game();
            
         
